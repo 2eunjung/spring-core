89403c0849876c48ed6128644e25a1e6fbbe8e43
2eunjung <12103207@gmail.com>
2025-01-12 19:03:04 +0900

    2025/01/12 스프링 입문 강의 내용 업로드 [섹션3] 스프링 핵심원리 이해 1 - 예제 만들기 [섹션4] 스프링 핵심원리 이해 2 - 객체 지향 원리 적용

---
+++ src/main/groovy/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,31 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    private final MemberRepository memberRepository;
+    // DIP 원칙에 위반됨
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // 추상화에만 의존하도록 코드 수정
+    private DiscountPolicy discountPolicy;
+
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
